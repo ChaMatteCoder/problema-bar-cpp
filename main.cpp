aa115c10ac95c5c32dff800c417f540f294cead3
Matheus Silva <matffernandesif@gmail.com>
2020-05-05 13:14:49 -0300

    leitura dos dados

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    string Sexo;
+    int Cervejas, Refrigerante, Espetos;
+
+    cout << "Sexo: ";
+    cin >> Sexo;
+    cout << "Quantidade de cervejas: ";
+    cin >> Cervejas;
+    cout << "Quantidade de refrigerantes: ";
+    cin >> Refrigerante;
+    cout << "Quantidade de espetinhos: ";
+    cin >> Espetos;
+
     return 0;
 }
